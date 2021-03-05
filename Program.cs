bd91be9aae82ed3eef2852de8dd329ac06ae9c39
LipatovValentin <ValentinLipatov@bk.ru>
2021-03-05 17:44:46 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new SimpleClass<int>(new[] { 1, 2, 3, 4, 5});
+            foreach (var element in array)
+            {
+                Console.WriteLine(element);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class SimpleClass<T>
+    {
+        private T[] _array;
+        private int _position = -1;
+        public T Current
+        {
+            get
+            {
+                try
+                {
+                    return this._array[this._position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                } 
+            }
+        }
+        public SimpleClass (params T[] values)
+        {
+            this._array = values;
+        }
+        public bool MoveNext()
+        {
+            this._position = this._position + 1;
+            if (this._position >= this._array.Length)
+            {
+                this._position = -1;
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public SimpleClass<T> GetEnumerator()
+        {
+            return this;
+        }
+    }
+}
